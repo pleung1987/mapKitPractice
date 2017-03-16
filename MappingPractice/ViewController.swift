6f77670daac9072d435cb13c404dd22677c5a838
Patrick Leung <pleung1987@gmail.com>
2017-03-17 05:30:52 +0800

    Initial Commit

---
+++ MappingPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MappingPractice
+//
+//  Created by Patrick Leung on 17/3/2017.
+//  Copyright © 2017 Patrick Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
